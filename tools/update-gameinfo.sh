e2dce060abada54238e652d70873664adea7cb90
Adrien Plazas <kekun.plazas@laposte.net>
2018-07-02 09:01:23 +0200

    tools: Comment why we sort in update-gameinfo.sh

--- tools/update-gameinfo.sh
+++ tools/update-gameinfo.sh
@@ -4,5 +4,8 @@ TMP_GAMEINFO_DOC='gameinfo/out/playstation.gameinfo.xml.in.tmp'
 GAMEINFO_DOC='../plugins/playstation/data/playstation.gameinfo.xml.in'
 
 gameinfo/psxdatacenter-gameinfo.py
+# Sort the existing document before merging the already sorted newly generated
+# one into it. This avoids inconsistencies in case the sorting function got
+# updated.
 gameinfo/sort.py $GAMEINFO_DOC
 gameinfo/merge.py $TMP_GAMEINFO_DOC $GAMEINFO_DOC $GAMEINFO_DOC
