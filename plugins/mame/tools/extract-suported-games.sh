efe4772f46258e40a8f62aa091a643cee702e0df
Alexander Mikhaylenko <alexm@gnome.org>
2020-02-06 02:34:33 +0500

    mame: Make extract-supported-games preprocess the file more
    Only leave id and name in. This will allow to drastically simplify and
    speed up the parsing in the next commit.

--- plugins/mame/tools/extract-suported-games.sh
+++ plugins/mame/tools/extract-suported-games.sh
@@ -3,4 +3,10 @@
 # $1: path to the MAME repository
 # $2: file name of the produced game database
 
-cat `find $1/src/mame/drivers -name "*.cpp" | sort` | egrep "^GAMEL?\(.*\)$" > $2
+simple=' *[^,]+ *'
+simpleg=' *([^,]+) *'
+quoted=' *" *(.*?) *" *'
+pattern="$simple,$simpleg,$simple,$simple,$simple,($simple,)?$simple,$simple,$quoted,$quoted,${simple}(${simple})?(,${simple})?"
+regex="^GAMEL?\(${pattern}\) *(//.*)?$"
+
+cat `find $1/src/mame/drivers -name "*.cpp" | sort` | egrep "^GAMEL?\(.*\)$" | sed -E "s|$regex|\1 \4|g" | sed -E 's/^([^ ]+) +(.+)$/\1 \2/g' | sort > $2
